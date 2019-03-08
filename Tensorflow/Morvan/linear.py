8bbc09a99f3ea51cf47b1e00dfa25925ab9a53f6
Tony Yang <yw981@sina.com>
2019-03-08 16:10:25 +0800

    add activation visualization

--- Tensorflow/Morvan/linear.py
+++ Tensorflow/Morvan/linear.py
@@ -11,6 +11,7 @@ plt.show()
 # exit(0)
 # print(x, y_Label)
 
+# tf.Variable是tf的变量，是会被训练优化的，非tf.Variable值不会变
 Weights = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
 biases = tf.Variable(tf.zeros([1]))
 
@@ -29,3 +30,4 @@ with tf.Session() as sess:
         sess.run(train)
         if step % 10 == 0:
             print(step, sess.run(loss), sess.run(Weights), sess.run(biases))
+
