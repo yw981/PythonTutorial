6cc7930e65e4a236440a48aedecbbac0f41ab68b
Tony Yang <yw981@sina.com>
2019-04-23 08:59:02 +0800

    finish error ev

---
+++ Tensorflow/5FullConnection/mnist_fc_softmax_error.py
@@ -0,0 +1,81 @@
+# coding:utf-8
+import time
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import mnist_forward
+import mnist_backward
+import numpy as np
+
+TEST_INTERVAL_SECS = 5
+
+
+# 必须先运行mnist_backward训练
+
+def test(mnist):
+    with tf.Graph().as_default() as g:
+        x = tf.placeholder(tf.float32, [None, mnist_forward.INPUT_NODE])
+        y_ = tf.placeholder(tf.float32, [None, mnist_forward.OUTPUT_NODE])
+        y = mnist_forward.forward(x, None)
+        # add softmax
+        y = tf.nn.softmax(y)
+
+        ema = tf.train.ExponentialMovingAverage(mnist_backward.MOVING_AVERAGE_DECAY)
+        ema_restore = ema.variables_to_restore()
+        saver = tf.train.Saver(ema_restore)
+
+        correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+        # 排序
+        y_sort = tf.contrib.framework.sort(y, 1, 'DESCENDING')
+
+        std = tf.constant([1.0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+        y_err = tf.reduce_sum(tf.math.square(y_sort - std),1)
+
+        y_err_right = tf.boolean_mask(y_err, correct_prediction)
+        y_err_wrong = tf.boolean_mask(y_err, ~correct_prediction)
+
+        while True:
+            with tf.Session() as sess:
+                ckpt = tf.train.get_checkpoint_state(mnist_backward.MODEL_SAVE_PATH)
+                if ckpt and ckpt.model_checkpoint_path:
+                    saver.restore(sess, ckpt.model_checkpoint_path)
+                    global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+                    # print(mnist.test.images)
+                    print(np.array(mnist.test.images).shape)
+                    # \ #
+
+                    accuracy_score, vy_sort, vy_err,vy_err_right,vy_err_wrong, vcr = sess.run([accuracy, y_sort, y_err,y_err_right,y_err_wrong, correct_prediction],
+                                                                    feed_dict={x: mnist.test.images,
+                                                                               y_: mnist.test.labels})
+                    print("After %s training step(s), test accuracy = %g" % (global_step, accuracy_score))
+                    print(vcr.shape)
+
+                    np.savetxt('vcr.npy', vcr)
+                    np.savetxt('vy_sort.npy', vy_sort)
+                    np.savetxt('vy_err.npy', vy_err)
+                    np.savetxt('vy_err_right.npy', vy_err_right)
+                    np.savetxt('vy_err_wrong.npy', vy_err_wrong)
+
+                    print('error_right min %g max %g mean %g var %g ' %
+                          (np.min(vy_err_right), np.max(vy_err_right), np.mean(vy_err_right), np.square(np.var(vy_err_right))))
+                    print('error_wrong min %g max %g mean %g var %g ' %
+                          (np.min(vy_err_wrong), np.max(vy_err_wrong), np.mean(vy_err_wrong),
+                           np.square(np.var(vy_err_wrong))))
+
+
+                else:
+                    print('No checkpoint file found')
+                    return
+            return
+            time.sleep(TEST_INTERVAL_SECS)
+
+
+def main():
+    mnist = input_data.read_data_sets("../data/", one_hot=True)
+    test(mnist)
+
+
+if __name__ == '__main__':
+    main()
--- Tensorflow/7Lenet5/test.py
+++ Tensorflow/7Lenet5/test.py
@@ -1,18 +1,18 @@
 import numpy as np
 
-r2 = np.loadtxt('vy_pred_right2')
+r2 = np.loadtxt('vy_pred_right2.npy')
 c1, c2 = np.split(r2, 2, axis=1)
 ratio_r = c1 / c2
 np.savetxt('ratio_right', ratio_r)
 print('ratio_right mean ', np.min(ratio_r), np.max(ratio_r), np.mean(ratio_r), np.var(ratio_r))
 print('it ', np.sum(ratio_r < 14))
 
-w2 = np.loadtxt('vy_pred_wrong2')
+w2 = np.loadtxt('vy_pred_wrong2.npy')
 c1, c2 = np.split(w2, 2, axis=1)
 ratio_w = c1 / c2
 np.savetxt('ratio_wrong', ratio_w)
 print('ratio_wrong mean ', np.min(ratio_w), np.max(ratio_w), np.mean(ratio_w), np.var(ratio_w))
 print('it ', np.sum(ratio_w < 14))
 
-for i in range(1, 100):
+for i in range(1, 200):
     print(np.sum(ratio_r < i), np.sum(ratio_w < i))
