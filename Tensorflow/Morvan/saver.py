a660ae889134f095ce3f5899f9c0f69af13688ae
Tony Yang <yw981@sina.com>
2019-06-18 09:45:10 +0800

    finish save restore

--- Tensorflow/Morvan/saver.py
+++ Tensorflow/Morvan/saver.py
@@ -1,10 +1,10 @@
 # View more python tutorials on my Youtube and Youku channel!!!
 
 import tensorflow as tf
-import numpy as np
 
 # Save to file
 # remember to define the same dtype and shape when restore
+
 W = tf.Variable([[1, 2, 3], [3, 4, 5]], dtype=tf.float32, name='weights')
 b = tf.Variable([[1, 2, 3]], dtype=tf.float32, name='biases')
 
@@ -24,15 +24,4 @@ with tf.Session() as sess:
 
 
 ################################################
-# restore variables
-# redefine the same shape and same type for your variables
-# W = tf.Variable(np.arange(6).reshape((2, 3)), dtype=tf.float32, name="weights")
-# b = tf.Variable(np.arange(3).reshape((1, 3)), dtype=tf.float32, name="biases")
-#
-# # not need init step
-#
-# saver = tf.train.Saver()
-# with tf.Session() as sess:
-#     saver.restore(sess, "my_net/save_net.ckpt")
-#     print("weights:", sess.run(W))
-#     print("biases:", sess.run(b))
+
